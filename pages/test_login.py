6547a7f47b2ff871872fe897d91c77eeb69fb26a
Maddy4201 <sonimadhur77@gmail.com>
2025-05-04 17:58:55 +0530

    defined element variables in login page

--- pages/test_login.py
+++ pages/test_login.py
@@ -7,8 +7,24 @@ from appium.options.android import UiAutomator2Options
 
 from pages.base_page import BasePage
 
-
 class UserLogin(BasePage):
+
+	initial_permission_xpath = "//android.widget.TextView[@resource-id='in.co.websites.websitesapp:id/btn_ok']"
+	location_access_xpath = "//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_foreground_only_button']"
+	allow_call_access_xpath = "//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_button']"
+	sign_in_button_xpath = "//android.widget.TextView[@resource-id='in.co.websites.websitesapp:id/btn_sign_in']"
+	email_field_xpath = "//android.widget.EditText[@resource-id='in.co.websites.websitesapp:id/email_login']"
+	password_field_xpath = "//android.widget.EditText[@resource-id='in.co.websites.websitesapp:id/password_login']"
+	login_button_xpath = "//android.widget.Button[@resource-id='in.co.websites.websitesapp:id/login_button']"
+	allow_notification_after_login_xpath = "//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_button']"
+	referral_pop_up_xpath = "//android.widget.ImageView[@content-desc='closeImage']"
+	sale_offer_pop_up_xpath = "//android.widget.ImageView[@resource-id='in.co.websites.websitesapp:id/close']"
+	menu_side_bar_xpath = "//android.widget.ImageButton[@content-desc='Open']"
+	buy_subscription_xpath = "//android.widget.TextView[@resource-id='in.co.websites.websitesapp:id/material_drawer_name' and @text='Buy Subscription']"
+	monthly_subscription_xpath = "(//android.widget.CheckBox[@content-desc='checkBox'])[1]"
+	subscribe_now_button_xpath = "//android.widget.TextView[@text='Subscribe Now']"
+	confirm_plan_xpath = "//android.widget.Button[@resource-id='in.co.websites.websitesapp:id/packageBtn']"
+	confirm_payment_button_xpath = "//android.widget.Button[@resource-id='in.co.websites.websitesapp:id/pay_button']"
 
 
 	def __init__(self):
@@ -17,34 +33,36 @@ class UserLogin(BasePage):
 # Wait for app to launch
 # time.sleep(3)
 	def user_login(self):
-		element = self.driver.find_element(AppiumBy.XPATH, "//android.widget.TextView[@resource-id='in.co.websites.websitesapp:id/btn_ok']")
-		element.click()
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_foreground_only_button']").click()
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_button']").click()
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.TextView[@resource-id='in.co.websites.websitesapp:id/btn_sign_in']").click()
-		self.driver.find_element(AppiumBy.XPATH, "//android.widget.EditText[@resource-id='in.co.websites.websitesapp:id/email_login']").send_keys("scott904@yopmail.com")
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.EditText[@resource-id='in.co.websites.websitesapp:id/password_login']").send_keys("Testuser@29")
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@resource-id='in.co.websites.websitesapp:id/login_button']").click()
+		self.driver.find_element(AppiumBy.XPATH, self.initial_permission_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.location_access_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.allow_call_access_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.sign_in_button_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.email_field_xpath).send_keys("miles604@yopmail.com")
+		self.driver.find_element(AppiumBy.XPATH, self.password_field_xpath).send_keys("Testuser@17")
+		self.driver.find_element(AppiumBy.XPATH, self.login_button_xpath).click()
 		# allow after login
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@resource-id='com.android.permissioncontroller:id/permission_allow_button']").click()
-		time.sleep(1)
-		# ignore you referral window
+		self.driver.find_element(AppiumBy.XPATH, self.allow_notification_after_login_xpath).click()
+		time.sleep(2)
+		# ignore referral pop-up
 		try:
-			referral_pop = self.driver.find_element(AppiumBy.XPATH,"//android.widget.ImageView[@content-desc='closeImage']")
+			referral_pop = self.driver.find_element(AppiumBy.XPATH, self.referral_pop_up_xpath)
 			if referral_pop.is_displayed():
 				referral_pop.click()
 			else:
 				print("Referral pop up not displayed")
 		except:
-			print("Refrerral pop-up ignored")
-		# click on sidebar
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.ImageButton[@content-desc='Open']").click()
-		# click on buy subcription
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.TextView[@resource-id='in.co.websites.websitesapp:id/material_drawer_name' and @text='Buy Subscription']").click()
-		self.driver.find_element(AppiumBy.XPATH,"(//android.widget.CheckBox[@content-desc='checkBox'])[1]").click()
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.TextView[@text='Subscribe Now']").click()
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@resource-id='in.co.websites.websitesapp:id/packageBtn']").click()
-		self.driver.find_element(AppiumBy.XPATH,"//android.widget.Button[@resource-id='in.co.websites.websitesapp:id/pay_button']").click()
+			print("Referral pop-up ignored")
+
+		time.sleep(2)
+		self.driver.find_element(AppiumBy.XPATH, self.sale_offer_pop_up_xpath).click()
+		# click on the sidebar
+		self.driver.find_element(AppiumBy.XPATH, self.menu_side_bar_xpath).click()
+		# click on buy subscription
+		self.driver.find_element(AppiumBy.XPATH, self.buy_subscription_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.monthly_subscription_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.subscribe_now_button_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.confirm_plan_xpath).click()
+		self.driver.find_element(AppiumBy.XPATH, self.confirm_payment_button_xpath).click()
 
 if __name__ == "__main__":
 	user = UserLogin()
